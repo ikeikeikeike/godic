f21f1e30423fa2263adc109e00b2e7d450d678f8
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-04-24 23:58:59 +0900

    Added loggign module

--- godic.go
+++ godic.go
@@ -1,6 +1,40 @@
 package main
 
-import "github.com/ikeikeikeike/godic/views"
+import (
+	"os"
+	"path"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/go-martini/martini"
+	"github.com/ikeikeikeike/godic/views"
+)
+
+func init() {
+	initLogger()
+}
+
+func initLogger() {
+	if martini.Env == "production" {
+		log.SetFormatter(&log.JSONFormatter{})
+		log.SetLevel(log.WarnLevel)
+
+		p, _ := os.Getwd()
+		file, err := os.OpenFile(
+			path.Join(p, "logs/godic.log"),
+			os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666,
+		)
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(file)
+	} else {
+		log.SetFormatter(&log.TextFormatter{ForceColors: true})
+		log.SetLevel(log.DebugLevel)
+		log.SetOutput(os.Stderr)
+	}
+
+	views.App.Map(log.StandardLogger())
+}
 
 func main() {
 	views.App.Run()
