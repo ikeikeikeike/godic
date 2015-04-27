1ec07b7178f1f952def9483a9ba61e0331aefdf5
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-04-27 20:28:31 +0900

    Added template functions

--- modules/funcmaps/appender.go
+++ modules/funcmaps/appender.go
@@ -1,1 +1,42 @@
 package funcmaps
+
+import (
+	"html"
+	"html/template"
+	"strings"
+	"time"
+)
+
+func ToAge(bd time.Time) int {
+	if bd.UnixNano() < 0 {
+		return 0
+	}
+
+	at := time.Now()
+	age := at.Year() - bd.Year()
+	if (at.Month() <= bd.Month()) && (at.Day() <= bd.Day()) {
+		age -= 1
+	}
+	return age
+}
+
+func SafeHTML(text string) template.HTML {
+	return template.HTML(text)
+}
+
+func EscapeHTML(in string) string {
+	return html.EscapeString(in)
+}
+
+func Nl2br(in string) string {
+	return strings.Replace(in, "\n", "<br>", -1)
+}
+
+var HelperFuncs = []template.FuncMap{
+	{
+		"toAge":    ToAge,
+		"safeHTML": SafeHTML,
+		"escapeHTML":   EscapeHTML,
+		"nl2br":    Nl2br,
+	},
+}
