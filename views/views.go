e869177adf7a38c1d13d7f255483d9aedb8f1e17
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-05-03 01:45:52 +0900

    next code

--- views/views.go
+++ views/views.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/binding"
+	"github.com/martini-contrib/oauth2"
 	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessions"
 
 	"github.com/ikeikeikeike/godic/middlewares/html"
 	"github.com/ikeikeikeike/godic/modules/funcmaps"
@@ -26,6 +28,29 @@ func init() {
 	App.Use(html.GenHTMLContext())
 	App.Use(html.HTMLHeader)
 	App.Use(html.HTMLMeta)
+
+	App.Use(sessions.Sessions(
+		"godic_sesssion", sessions.NewCookieStore([]byte("secret09131ffl2"))),
+	)
+
+	App.Use(oauth2.Github(&goauth2.Config{
+		ClientID:     "0.0",
+		ClientSecret: "o.o",
+		// RedirectURL:  "http://localhost:3000/oauth2callback",
+		Scopes: []string{"user:email", "read:org"},
+	}))
+	App.Use(oauth2.Facebook(&goauth2.Config{
+		ClientID:     "0.0",
+		ClientSecret: "o.o",
+		// RedirectURL:  "http://localhost:3000/oauth2callback",
+		Scopes: []string{"user:email", "read:org"},
+	}))
+	App.Use(oauth2.Google(&goauth2.Config{
+		ClientID:     "",
+		ClientSecret: "",
+		// RedirectURL:  "http://localhost:3000/oauth2callback",
+		Scopes: []string{"https://www.googleapis.com/auth/drive"},
+	}))
 
 	App.Get("/", func(r render.Render) { r.Redirect("/d/index") }).Name("root")
 
