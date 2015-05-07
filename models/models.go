126b2881e40db9255de1ed6a19e361f9805e7b6c
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-05-08 06:01:12 +0900

    modify seed data for development

--- models/models.go
+++ models/models.go
@@ -1,10 +1,15 @@
 package models
 
 import (
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"time"
 
 	"github.com/go-martini/martini"
+	"github.com/ikeikeikeike/gopkg/rdm"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
@@ -50,15 +55,62 @@ func init() {
 
 func InitSeed() {
 	cate := new(Category)
+	DB.Where(Category{Name: "ノンカテゴリー"}).
+		Attrs(Category{Yomi: "のんかてごり", Romaji: "nonkategori", Gyou: "no", Prefix: "none"}).
+		FirstOrCreate(&cate)
+
+	cate = new(Category)
 	DB.Where(Category{Name: "アイドル・女優"}).
 		Attrs(Category{Yomi: "あいどるじょゆう", Romaji: "aidorujoyu", Gyou: "a", Prefix: "diva"}).
 		FirstOrCreate(&cate)
+
 	cate = new(Category)
 	DB.Where(Category{Name: "漫画・アニメ"}).
 		Attrs(Category{Yomi: "まんがあにめ", Romaji: "mangaanime", Gyou: "ma", Prefix: "anime"}).
 		FirstOrCreate(&cate)
+
 	cate = new(Category)
 	DB.Where(Category{Name: "漫画・アニメキャラ"}).
 		Attrs(Category{Yomi: "まんがあにめきゃら", Romaji: "mangaanimekyara", Gyou: "ma", Prefix: "character"}).
 		FirstOrCreate(&cate)
+
+	if martini.Env != "production" {
+
+		basePath, _ := os.Getwd()
+		bytes, err := ioutil.ReadFile(path.Join(basePath, "template.txt"))
+
+		i := 0
+		for i < 100 {
+			name := letterCombinePtn(7)
+
+			content := ""
+			if err == nil {
+				content = fmt.Sprintf(string(bytes), name)
+			}
+
+			c := &Category{}
+			DB.First(c, int64(rdm.RandomNumber(1, 5)))
+
+			d := new(Dict)
+			DB.Where(Dict{Name: name}).
+				Attrs(Dict{
+				Yomi:     letterCombinePtn(3),
+				Prefix:   letterCombinePtn(7),
+				Content:  content,
+				Category: c,
+			}).
+				FirstOrCreate(&d)
+			i++
+		}
+	}
+}
+
+func letterCombinePtn(n int) string {
+	const letters = "abcdefg" // 7P7=7*6*5*4*3*2*1=5040
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes)
 }
