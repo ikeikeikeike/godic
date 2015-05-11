80a8e4ab5a54aa9fbd25d9407d27d2f2d47e570b
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-05-11 16:25:27 +0900

    more code for ToCanonical function

--- modules/funcmaps/http.go
+++ modules/funcmaps/http.go
@@ -24,6 +24,8 @@ func ToCanonical(name string) string {
 	name = strings.Replace(name, `^`, "＾", -1)
 	name = strings.Replace(name, `$`, "＄", -1)
 	name = strings.Replace(name, `@`, "＠", -1)
+	name = strings.Replace(name, "\u3000", " ", -1)
+	name = strings.Replace(name, "　", " ", -1)
 	name = strings.Replace(name, " ", "_", -1)
 	return name
 }
